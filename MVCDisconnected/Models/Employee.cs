28432cd70da0dbb637528fcb680d06d854142e9e
Vamshi Kaude <choice_vamshi.k@outlook.com>
2025-01-28 20:43:03 +0530

    Add project files.

---
+++ MVCDisconnected/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDisconnected.Models
+{
+    public class Employee
+    {
+        public int Eno { get; set; }
+        public string Ename {  get; set; }
+        public string Job {  get; set; }
+        public double Salary {  get; set; }
+        public string Dname {  get; set; }
+    }
+}
